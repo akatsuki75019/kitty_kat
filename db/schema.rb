13599cbfac49311bd3a2b4664388dcd725728450
Sam <146119225+Samalzdh@users.noreply.github.com>
2023-11-27 15:39:49 +0100

    correction conflit des tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-table_user
 ActiveRecord::Schema[7.1].define(version: 2023_11_27_135658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,9 +25,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_135658) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_135628) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -37,7 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_135628) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
- development
+
   end
 
 end
