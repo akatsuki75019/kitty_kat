203e4ccbeb503b5dff6b9f5ac0885bb14a7957ed
nellotte <nelly.guerin@gmail.com>
2023-11-27 23:46:43 +0100

    création automatique du cart apres la création d'un nouvel user s'il n'a pas de cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  include ApplicationHelper
+  after_action :create_cart_and_assign_to_current_user, only: [:create]
+  #appelle de la methode "create cart &assign" apres que l'action de création soit terminée
 end
