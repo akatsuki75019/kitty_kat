8c1d462fbb68d57c0e54f35e0af7ca43281f99f8
AxelCourtois <courtois.a@hotmail.com>
2023-11-29 16:35:23 +0100

    Gn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_160527) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_113807) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cart_items", force: :cascade do |t|tp
+  create_table "cart_items", force: :cascade do |t|
     t.bigint "cart_id", null: false
     t.bigint "item_id", null: false
     t.datetime "created_at", null: false
@@ -42,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_27_160527) do
   create_table "order_items", force: :cascade do |t|
     t.bigint "order_id", null: false
     t.bigint "item_id", null: false
-    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_order_items_on_item_id"
