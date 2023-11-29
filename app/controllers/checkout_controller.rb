366f05edeb6042ff7ba639977f7bc051f7e8eb42
nellotte <nelly.guerin@gmail.com>
2023-11-29 20:15:37 +0100

    paiement stripe d'un panier contenent plusieurs items : OK

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,8 +1,13 @@
 class CheckoutController < ApplicationController
   def create
     @total = params[:total].to_d
-    @item_id = params[:item_id]
     @user_id = params[:user_id]
+    @cart_items = current_user.cart.cart_items.includes(:item)
+    total_price = @cart_items.sum { |cart_item| cart_item.item.price }
+  
+    cart_item_ids = @cart_items.pluck(:id)
+  
+
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [
@@ -18,8 +23,9 @@ class CheckoutController < ApplicationController
         },
       ],
       metadata: {
-        item_id: @item_id,
+        cart_item_ids: cart_item_ids.join(','),
         user_id: @user_id
+       
       },
       mode: 'payment',
       customer_email: current_user.email, 
@@ -34,16 +40,25 @@ class CheckoutController < ApplicationController
 
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-    @item_id = @session.metadata.item_id 
-
+    cart_item_ids = @session.metadata.cart_item_ids
+   
 
     # Création d'une nouvelle instance dans la BDD Order
     @order = Order.create(user_id: current_user.id)
 
-    #Création des instances dans la table jointe order_items
-    OrderItem.create(order_id: @order.id, item_id: @item_id)
+    
+    # Récupération des IDs des cart_items depuis les métadonnées
+    cart_item_ids = @session.metadata.cart_item_ids.split(',').map(&:to_i)
 
-     
+
+    #Création des instances dans la table jointe order_items
+    cart_item_ids.each do |cart_item_id|
+      cart_item = CartItem.find(cart_item_id)
+      OrderItem.create(order_id: @order.id, item_id: cart_item.item_id)
+    end
+
+    # Effacer les articles du panier après la commande
+      current_user.cart.cart_items.destroy_all
   end
 
 
