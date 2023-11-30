8c66e7d1d7d97ad28020e5e2b2f1f9f9b4d4d458
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-11-30 14:38:17 +0100

    add image array into the seed so we can generate aleatory images with your own collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,14 +23,18 @@ team_players.shuffle.each do |player|
     password: password)
 end
 
+#Mon stock d'images celui contenu dans assets/images
+image_array = ["avatar 2.jpeg", "avatar 3.jpeg", "avatar 4.jpeg", "cat .png", "cat 2.jpeg", "cat 3.jpeg", "cat chill.jpeg", "cat reading.jpeg", "cat samurai 1.webp", "cat samurai 2.jpeg", "chat de race.jpeg", "chat long poils.webp", "chat samurai 1.png", "chaton_achat.png", "chaton_sur-dos.jpeg", "christmas_cat_sm.webp", "cute_cat_square.jpeg", "fat_cat.jpg", "funny_cat_sm.jpeg", "kittens.jpeg", "kitty_cat.jpeg", "rock_cat_sm.avif", "sad_cat.jpeg", "Sphynx.webp"]
 
+# Pour mixer un peu l'ordre des images
+shuffled_images = image_array.shuffle
 
-10.times do
+24.times do |image|
   Item.create(
     title: Faker::Commerce.product_name,
     description: Faker::Lorem.paragraph,
     price: Faker::Commerce.price(range: 1.00..20.00),
-    image_url: Faker::LoremFlickr.image(size: "300x300", search_terms: ['item'])
+    image_url: shuffled_images[image]
   )
 end
 
