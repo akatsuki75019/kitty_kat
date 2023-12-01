b658fe7963de2d835ccb8d1847b2cf624fce1a93
Sam <146119225+Samalzdh@users.noreply.github.com>
2023-12-01 12:57:33 +0100

    tentative resolution pb

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,17 +1,19 @@
-# app/mailers/order_mailer.rb
 class OrderMailer < ApplicationMailer
   default from: 'courtois.a@hotmail.com'
+
   def order_confirmation(user, order)
     @user = user
     @order = order
-    mail(to: @user.email, subject: 'Récapitulatif de votre commande')
-      # Ajouter les pièces jointes ici si nécessaire
-      @order.items.each do |item|
-        image_path = "app/assets/images/#{item.cart.image_filename}"
-        if File.exist?(image_path)
-          attachments[item.cart.image_filename] = File.read(image_path)
-        else
-          Rails.logger.warn("Image not found at #{image_path}")
+    mail(to: @user.email, subject: 'Récapitulatif de votre commande') do |format|
+      format.html do
+        # Ajouter les pièces jointes ici si nécessaire
+        @order.items.each do |item|
+          image_path = "app/assets/images/#{item.cart.image_filename}"
+          if File.exist?(image_path)
+            attachments[item.cart.image_filename] = File.read(image_path)
+          else
+            Rails.logger.warn("Image not found at #{image_path}")
+          end
         end
       end
     end
